892e7f86d645ba7fe0c1796ea413febd00d0eded
Alexey K <uuuuiiiaaa@yandex.ru>
2020-03-24 01:09:29 +0300

    Add base model for app

--- backend/src/project/models.py
+++ backend/src/project/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    maked = models.DateTimeField('Создано', auto_now_add=True)
+    created = models.DateTimeField('Создано', auto_now_add=True)
     updated = models.DateTimeField('Обновлено', auto_now=True)
 
     class Meta:
         abstract = True
-
