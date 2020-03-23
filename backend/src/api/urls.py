522fb690ba170fc85b1870da1615876a3a418b2e
Alexey K <uuuuiiiaaa@yandex.ru>
2020-03-23 22:43:15 +0300

    Add api url patterns with auth stuff

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -1,3 +1,15 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 
-urlpatterns = []
+from rest_framework.schemas import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('', include('applications.urls')),
+    path('shema/', get_schema_view()),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth/get_token/', TokenObtainPairView.as_view()),
+    path('auth/refresh_token/', TokenRefreshView.as_view()),
+]
