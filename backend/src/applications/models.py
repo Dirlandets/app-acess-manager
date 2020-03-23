1afd157d5a27c0c10b5053c50615f67c6a11236f
Alexey K <uuuuiiiaaa@yandex.ru>
2020-03-24 01:09:08 +0300

    Add model for Application

--- backend/src/applications/models.py
+++ backend/src/applications/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from project.models import BaseModel
+
+
+class Application(BaseModel):
+    owner = models.ForeignKey(
+        User,
+        'Владелец приложения',
+        null=False,
+        blank=False,
+    )
+    name = models.CharField(
+        'Название приложения',
+        max_length=200,
+        null=False,
+        blank=False,
+        default=None,
+    )
+    api_key = models.UUIDField(
+        'Ключ API',
+        default=uuid.uuid4,
+        unique=True
+    )
+
+    def refresh_key(self):
+        self.api_key = uuid.uuid4()
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        '''
+        Make sure the key is unique
+        '''
+        verbose_name = 'Приложение'
+        verbose_name_plural = 'Приложения'
