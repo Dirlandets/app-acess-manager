54af635883c8a6e3f2e7ea7d219eae9df24e6e49
Alexey K <uuuuiiiaaa@yandex.ru>
2020-03-24 13:03:52 +0300

    Add all views

--- backend/src/applications/views.py
+++ backend/src/applications/views.py
@@ -1,11 +1,15 @@
 from django.db.models import QuerySet
-from django.views.decorators.http import require_http_methods
+from django.shortcuts import redirect
 from rest_framework.generics import (
     RetrieveUpdateDestroyAPIView,
     ListCreateAPIView,
 )
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+
+from rest_framework.permissions import IsAuthenticated
+
+from api.permissions import TokenIsActivePermission
 
 from .models import Application
 from .serializers import ApplicationSerrializer
@@ -14,6 +18,7 @@ from .serializers import ApplicationSerrializer
 class ApplicationListView(ListCreateAPIView):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerrializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self) -> 'QuerySet[Application]':
         user = self.request.user
@@ -32,6 +37,7 @@ class ApplicationListView(ListCreateAPIView):
 class ApplicationView(RetrieveUpdateDestroyAPIView):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerrializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         '''Return only objects owned by user if not superuser'''
@@ -41,3 +47,43 @@ class ApplicationView(RetrieveUpdateDestroyAPIView):
         else:
             qs = self.queryset
         return qs
+
+
+class RefreshApplicationToken(APIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerrializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        '''Return only objects owned by user if not superuser'''
+        # TODO: Сделать permission для проверки.
+        user = self.request.user
+        if not user.is_superuser:
+            qs = self.queryset.filter(owner=user)
+        else:
+            qs = self.queryset
+        return qs
+
+    def get(self, request, pk):
+        try:
+            for_refresh = self.get_queryset().get(pk=int(pk))
+            for_refresh.refresh_key()
+            response = redirect(f'/api/{for_refresh.pk}/')
+        except Application.DoesNotExist:
+            response = Response({'error': 'You can\'t do that'})
+        return response
+
+
+class TestTokenView(APIView):
+    """
+    View to test token is valid and worked fine.
+    """
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerrializer
+    permission_classes = [TokenIsActivePermission]
+
+    def get(self, request, format=None):
+        token = self.request.headers.get('Token', None)
+        app = self.queryset.get(api_key=token)
+        serializer = self.serializer_class(app)
+        return Response(serializer.data)
