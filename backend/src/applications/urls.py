c5cef0a0a04f2a432e597ca3302e42c46787d372
Alexey K <uuuuiiiaaa@yandex.ru>
2020-03-24 12:52:05 +0300

    Add urls for views

--- backend/src/applications/urls.py
+++ backend/src/applications/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import ApplicationView, ApplicationListView
+from .views import (
+    ApplicationView,
+    ApplicationListView,
+    TestTokenView,
+    RefreshApplicationToken
+)
 
 urlpatterns = [
+    path('test/', TestTokenView.as_view()),
     path('', ApplicationListView.as_view()),
     path('<pk>/', ApplicationView.as_view()),
+    path('<pk>/refresh/', RefreshApplicationToken.as_view()),
 ]
